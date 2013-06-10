60dc33b48f82a84d7ced2d6d4b9c8c9ff7211fe0
lorenzo_vinci <silent.lory@gmail.com>
2013-06-10 10:38:51 +0200

    corretto maintenaceTypeDAO

--- RONF/src/it/unibo/ronf/server/dao/MaintenanceTypeDAO.java
+++ RONF/src/it/unibo/ronf/server/dao/MaintenanceTypeDAO.java
@@ -12,7 +12,7 @@ public class MaintenanceTypeDAO extends JpaDAO<MaintenanceType> {
 	public MaintenanceType findByName(String name) {
 		
 		TypedQuery<MaintenanceType> query = em.createQuery(
-				"SELECT mt FROM MaintenanceType a WHERE mt.name = :name", entityClass);
+				"SELECT mt FROM MaintenanceType mt WHERE mt.name = :name", entityClass);
 		
 		query.setParameter("name", name);
 		
